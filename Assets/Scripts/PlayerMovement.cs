a8cf168129c79afb40689fef05068f2f21fec966
Anthony Simon Vadakara <vadakaraanthony96@gmail.com>
2022-07-14 17:56:00 -0400

    7/14

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 direction;
+    public float speed = 1;
+    Vector3 location;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        direction = new Vector3(x, 0, y);
+
+        transform.position += (direction * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TileConverter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class TileConverter : MonoBehaviour
+{
+    [System.Serializable]
+    public struct TileRule
+    {
+        public Tile tile;
+        public GameObject obj;
+    } 
+
+    public Tilemap map;
+    Vector3 offset;
+    public List<TileRule> rules;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (map == null)
+        {
+            map = transform.parent.GetComponent<Tilemap>();
+        }
+
+        Dictionary<Tile, GameObject> dictionary = new Dictionary<Tile, GameObject>();
+        foreach (TileRule rule in rules)
+        {
+            dictionary.Add(rule.tile, rule.obj);
+        }
+
+        BoundsInt bounds = map.cellBounds;
+        TileBase[] tiles = map.GetTilesBlock(bounds);
+        offset = (map.origin * 2) + (new Vector3(map.transform.position.x, map.transform.position.z, 0)*2) ;        
+        offset.z = offset.y;
+        offset.y = 0;
+        print(map.origin);
+        print(offset);
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                Tile tile = (Tile)tiles[x + y * bounds.size.x];
+                Vector3 pos = new Vector3(x * 2, 0, y * 2) + offset;
+                if (tile != null && dictionary.ContainsKey(tile))
+                {
+                    Instantiate(dictionary[tile], pos, Quaternion.identity);
+                }
+            }
+        }
+        map.GetComponent<TilemapRenderer>().enabled = false;
+    }
+}
